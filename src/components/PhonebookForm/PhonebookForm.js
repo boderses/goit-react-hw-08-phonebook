29adcc7c0f10b574f960775983c7b8c8992afd92
Bohdan Ses <bohdan.ses@gmail.com>
2022-11-20 18:15:55 +0100

    add validation for phonebook form

--- src/components/PhonebookForm/PhonebookForm.js
+++ src/components/PhonebookForm/PhonebookForm.js
@@ -1,20 +1,41 @@
 import { useDispatch } from 'react-redux';
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { addContact } from '../../redux/contacts/thunks';
-import { nanoid } from 'nanoid';
 import { Button, TextField, Box, Typography } from '@mui/material';
+import { useEffect } from 'react';
 
-const PhonebookForm = ({ classes }) => {
+const schema = yup.object({
+  name: yup.string().required('This field is required'),
+  number: yup
+    .number()
+    .typeError('Value must be a positive number')
+    .positive('Value must be a positive number')
+    .integer('Value must be an integer')
+    .required('This field is required'),
+});
+
+const PhonebookForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   const dispatch = useDispatch();
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const newContact = {
-      id: nanoid(),
-      name: event.currentTarget.elements.name.value,
-      number: event.currentTarget.elements.number.value,
-    };
-    dispatch(addContact(newContact));
-    event.currentTarget.reset();
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSubmitSuccessful]);
+
+  const onSubmitHandler = values => {
+    dispatch(addContact(values));
   };
 
   return (
@@ -22,32 +43,50 @@ const PhonebookForm = ({ classes }) => {
       <Typography component="h1" variant="h5">
         Phonebook
       </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmitHandler)}
+        sx={{ mt: 1 }}
+      >
+        <Controller
+          render={({ field }) => (
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="name"
+              label="Name"
+              type="name"
+              id="name"
+              autoComplete="name"
+              autoFocus
+              {...field}
+            />
+          )}
           name="name"
-          label="Name"
-          type="name"
-          id="name"
-          autoComplete="name"
-          autoFocus
+          control={control}
+          defaultValue={''}
         />
-        <TextField
-          inputProps={{
-            pattern:
-              '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}',
-          }}
-          margin="normal"
-          type="tel"
-          required
-          fullWidth
-          id="number"
-          label="Phone number"
+        {errors.name && <p>{errors.name.message}</p>}
+        <Controller
+          render={({ field }) => (
+            <TextField
+              margin="normal"
+              type="tel"
+              required
+              fullWidth
+              id="number"
+              label="Phone number"
+              name="number"
+              autoComplete="number"
+              {...field}
+            />
+          )}
           name="number"
-          autoComplete="number"
+          control={control}
+          defaultValue={''}
         />
+        {errors.number && <p>{errors.number.message}</p>}
 
         <Button
           type="submit"
